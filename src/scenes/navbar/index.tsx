30baea82618e7430733893d268b80e23a55fe7e3
khalidur-sujon <khalidur.sujon@gmail.com>
2023-10-21 21:24:17 +0600

    fixing typing mistake

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -38,7 +38,7 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }: Props) => {
 										setSelectedPage={setSelectedPage}
 									/>
 									<Link
-										page="Benefits"
+										page="Benifits"
 										selectedPage={selectedPage}
 										setSelectedPage={setSelectedPage}
 									/>
